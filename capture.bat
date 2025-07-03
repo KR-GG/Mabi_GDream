5d6c3a26552bd94382465720d12a3cbf3922fd86
KR-GG <150109746+KR-GG@users.noreply.github.com>
2025-07-04 02:47:34 +0900

    v-1.1.0(최적화)

--- capture.bat
+++ capture.bat
@@ -4,7 +4,7 @@ setlocal
 set VENV_DIR=gdream
 set PYTHON_FILE=./source/capture.py
 set HTML_FILE=stats.html
-set CHROME_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe" REM Adjust this path if Chrome is installed elsewhere
+set CHROME_PATH="C:\Program Files\Google\Chrome\Application\chrome.exe"
 
 if not exist %VENV_DIR% (
     echo Creating virtual environment...
@@ -13,9 +13,9 @@ if not exist %VENV_DIR% (
 
 call %VENV_DIR%\Scripts\activate.bat
 
-start "" %CHROME_PATH% "%CD%\%HTML_FILE%"
-
 pip install --disable-pip-version-check --quiet websockets scapy brotli
+
+start "" %CHROME_PATH% "%CD%\%HTML_FILE%"
 
 python %PYTHON_FILE%
 
