042644a2cef99443b571cd6ac93730e2f8923c7f
KR-GG <150109746+KR-GG@users.noreply.github.com>
2025-07-15 21:44:41 +0900

    v-1.1.3(디버그 모드)

--- capture.bat
+++ capture.bat
@@ -23,28 +23,23 @@ if %errorlevel% neq 0 (
     echo [INFO] Firewall rule for TCP port 8000 already exists.
 )
 
-:: 3. Start hidden background ping task (to maintain ARP entry)
-:: This pings 192.168.219.108 every 60s, silently
-start "" /min cmd /c ^
-    "for /L %%i in (1,0,2) do (ping -n 2 192.168.219.108 >nul & timeout /t 60 >nul)"
-
-:: 4. Set file paths
+:: 3. Set file paths
 set VENV_DIR=gdream
 set PYTHON_FILE=.\source\capture.py
 
-:: 5. Create virtual environment if missing
+:: 4. Create virtual environment if missing
 if not exist %VENV_DIR% (
     echo [INFO] Creating virtual environment...
     python -m venv %VENV_DIR%
 )
 
-:: 6. Activate virtual environment
+:: 5. Activate virtual environment
 call %VENV_DIR%\Scripts\activate.bat
 
-:: 7. Install dependencies
+:: 6. Install dependencies
 pip install --disable-pip-version-check --quiet websockets scapy brotli
 
-:: 8. Run Python WebSocket server
+:: 7. Run Python WebSocket server
 python %PYTHON_FILE%
 
 endlocal
